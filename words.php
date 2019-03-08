6fd127fd869fb63f7cf0b1aeb86efba8f3a36806
Евгений <quemsaurose@gmail.com>
2019-03-08 18:29:52 +0300

    Update words.php

--- words.php
+++ words.php
@@ -1,8 +1,7 @@
 <?php
 $bot_hello = array();
-$bot_hello[] = 'И тебе привет';
-$bot_hello[] = 'Здарова';
-$bot_hello[] = 'Доброго времени суток';
-$bot_hello[] = 'Привет-привет че надо';
+$bot_hello[] = 'Beep Boop I am Robot.';
+$bot_hello[] = 'Здарова кожаные ублюдки!';
+$bot_hello[] = 'Beep boop, son. Beep boop.';
 $message = $bot_hello[rand(0, (count($bot_hello) - 1)) ];
 ?>
