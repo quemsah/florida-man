d68f7e092c23281d6e5eacca0915544fddb80596
Евгений <quemsaurose@gmail.com>
2019-03-09 00:17:21 +0300

    /

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
-require 'words.php';
-function FloridaMan($i)
-{
-    $a = Array(
-        'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
-        'QGZsb3JpZGFfbWFu'
-    );
-    return base64_decode($a[$i]);
-}
-$botToken  = FloridaMan(0);
+require 'define.php';
+
+$bot_token  = FloridaMan(0);
 $channel  = FloridaMan(1);
-$bot_url = "https://api.telegram.org/bot$botToken/";
-$url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($message);
-file_get_contents($url);
+
+$bot_answer = array();
+$bot_answer[] = 'Beep Boop I am Robot.';
+$bot_answer[] = 'Здарова кожаные ублюдки!';
+$bot_answer[] = 'Beep boop, son. Beep boop.';
+
+$bot_url = "https://api.telegram.org/bot$bot_token/";
+$message = $bot_answer[rand(0, (count($bot_answer) - 1)) ];
+//$url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($message);
+//file_get_contents($url);
+echo $message;
+
+
+
+$output = json_decode(file_get_contents('php://input'), TRUE);
+echo $output;
