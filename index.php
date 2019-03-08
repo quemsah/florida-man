b183aa260f5d1551aa8abac93f5f6ee22a64f3fd
Евгений <quemsaurose@gmail.com>
2019-03-08 17:56:39 +0300

    пусть пишет разные слова в канал, для начала

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
+require 'words.php';  // Работает
 $botToken = "794825320:AAHYm4zGhBcvlAnqwX-oQ9lJwxKiOlX2Bl0";
 $chat_id = "@florida_man";
-$message = "Ну что кожаные ублюдки обосрались";
+
 $bot_url = "https://api.telegram.org/bot$botToken/";
 $url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
 file_get_contents($url);
