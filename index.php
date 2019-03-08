2df2c8ca4c8bc83a1cfd6433d43df0695b7355ac
Евгений <quemsaurose@gmail.com>
2019-03-09 00:27:35 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require 'define.php';
 
 $bot_token  = FloridaMan(0);
 $channel  = FloridaMan(1);
+<<<<<<< HEAD
 
 $bot_answer = array();
 $bot_answer[] = 'Beep Boop I am Robot.';
@@ -21,3 +22,16 @@ echo $message;
 $output = implode(",", json_decode(file_get_contents('php://input'), TRUE));
 $url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($output);
 file_get_contents($url);
+=======
+$bot_url = "https://api.telegram.org/bot$botToken/";
+$fp = fopen("is_running", "r+");
+if (! flock($fp, LOCK_EX | LOCK_NB)) return;
+for (;;)
+{
+  perform_actions();
+  sleep(300);
+  $url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($message);
+  file_get_contents($url);
+}
+?>
+>>>>>>> parent of 1d774f3... Update index.php
