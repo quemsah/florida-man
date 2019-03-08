3d76b3428eb63450d5da8261da1f7cd7afe9b31d
Евгений <quemsaurose@gmail.com>
2019-03-08 18:26:05 +0300

    /

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
-require 'words.php';  // Работает
-$botToken = "794825320:AAHYm4zGhBcvlAnqwX-oQ9lJwxKiOlX2Bl0";
-$chat_id = "@florida_man";
-
+require 'words.php';
+function FloridaMan($i)
+{
+    $a = Array(
+        'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
+        'QGZsb3JpZGFfbWFu'
+    );
+    return base64_decode($a[$i]);
+}
+$botToken  = FloridaMan(0);
+$channel  = FloridaMan(1);
 $bot_url = "https://api.telegram.org/bot$botToken/";
-$url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
+$url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($message);
 file_get_contents($url);
 ?>
-
-
--- words.php
+++ words.php
@@ -1,12 +1,8 @@
 <?php
-  $bot_hello = array();
-  $bot_hello[] = 'И тебе привет';
-  $bot_hello[] = 'Здарова';
-  $bot_hello[] = 'Доброго времени суток';
-  $bot_hello[] = 'Привет-привет че надо';
-    //пользователь поздоровался.
-    //случайная фраза привет от бота
-    $message = $bot_hello[rand(0, (count($bot_hello) - 1))];
+$bot_hello = array();
+$bot_hello[] = 'И тебе привет';
+$bot_hello[] = 'Здарова';
+$bot_hello[] = 'Доброго времени суток';
+$bot_hello[] = 'Привет-привет че надо';
+$message = $bot_hello[rand(0, (count($bot_hello) - 1)) ];
 ?>
-
-
