0926ef28c329bc11d9b9a565bdcd8dd87c1974a6
Евгений <quemsaurose@gmail.com>
2019-03-08 17:38:39 +0300

    удалил вежливое приветсвие, теперь боту нельзя написать

--- functions.php
+++ functions.php
@@ -1,37 +1,4 @@
 <?php
-function getUserRequest($text, $chat_id) {
-  //список приветствий, который понимает бот
-  $hello = array();
-  $hello[] = 'Привет';
-  $hello[] = 'привет';
-  $hello[] = 'Здравствуйте';
 
-  // варианты ответов бота
-  $bot_hello = array();
-  $bot_hello[] = 'И тебе привет';
-  $bot_hello[] = 'Здарова';
-  $bot_hello[] = 'Доброго времени суток';
-  $bot_hello[] = 'Привет-привет че надо';
 
-  if (in_array(strtolower($text), $hello)) {
-    //пользователь поздоровался.
-    //случайная фраза привет от бота
-    $bot_resp = $bot_hello[rand(0, (count($bot_hello) - 1))];
-    $data = array(
-      'text' => $bot_resp,
-      'chat_id' => $chat_id,
-    );
-    requestToTelegram($data);
-  }
-}
-function requestToTelegram($data, $type = 'sendMessage') {
-  if( $curl = curl_init() ) {
-    curl_setopt($curl, CURLOPT_URL, API_URL . $type);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($curl, CURLOPT_POST, TRUE);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-    curl_exec($curl);
-    curl_close($curl);
-  }
-}
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
-// листинг bot.php
-//задаём наш токен, полученный при создании бота и указываем путь к API телеграма
-require 'define.php';  // Работает
-include_once 'functions.php';
-//принимаем запрос от бота(то что напишет в чате пользователь)
-$content = file_get_contents('php://input');
-//превращаем из json в массив
-$update = json_decode($content, TRUE);
-//получаем id чата
-$chat_id = $update['message']['chat']['id'];
-//получаем текст запроса
-$text = $update['message']['text'];
-//обработка запроса
-getUserRequest($text, $chat_id);
+$botToken = "794825320:AAHYm4zGhBcvlAnqwX-oQ9lJwxKiOlX2Bl0";
+$chat_id = "@florida_man";
+$message = "Ну что кожаные ублюдки обосрались";
+$bot_url = "https://api.telegram.org/bot$botToken/";
+$url = $bot_url."sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
+file_get_contents($url);
 ?>
+
 
