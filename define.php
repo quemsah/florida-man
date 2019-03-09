fcf981d443d192bd73583bd46dd400c6bbd57387
Евгений <quemsaurose@gmail.com>
2019-03-09 13:30:59 +0300

    /

---
+++ define.php
@@ -0,0 +1,10 @@
+<?php
+function FloridaMan($i)
+{
+    $a = Array(
+        'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
+        'QGZsb3JpZGFfbWFu'
+    );
+    return base64_decode($a[$i]);
+}
+?>
--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-require 'words.php';
-function FloridaMan($i)
-{
-    $a = Array(
-        'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
-        'QGZsb3JpZGFfbWFu'
-    );
-    return base64_decode($a[$i]);
-}
-$botToken  = FloridaMan(0);
+require 'define.php';
+include_once( "/inc/telegramDebugger/debug.inc" );
+
+$bot_token  = FloridaMan(0);
 $channel  = FloridaMan(1);
-$bot_url = "https://api.telegram.org/bot$botToken/";
+
+$bot_answer = array();
+$bot_answer[] = 'Beep Boop I am Robot.';
+$bot_answer[] = 'Здарова кожаные ублюдки!';
+$bot_answer[] = 'Beep boop, son. Beep boop.';
+
+$bot_url = "https://api.telegram.org/bot$bot_token/";
+$message = $bot_answer[rand(0, (count($bot_answer) - 1)) ];
 $url = $bot_url . "sendMessage?chat_id=" . $channel . "&text=" . urlencode($message);
 file_get_contents($url);
-?>
