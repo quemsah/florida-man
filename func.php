1230214343fd0f1e3c9e8b8e45a0c2ee53b8d3cd
Евгений <quemsaurose@gmail.com>
2019-03-11 01:57:48 +0300

    бот знает кто и что ему написал и любезно транслирует это в канал

--- func.php
+++ func.php
@@ -1,38 +1,47 @@
 <?php
- function floridaMan($i)
-{
-    $a = Array(
-        'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
-        'LTEwMDE0MTg0NDU0MDU='
-    );
-    return base64_decode($a[$i]);
-}
-function toUser($text, $user_data) {
-    $chat_id = $user_data['message']['chat']['id'];
-    $text = $user_data['message']['text'];
-    $bot_response = 'Я тебя услышал, ты написал: "' . $text . '". ' . 'Держи случайное число: ' . rand(1, 1000000);
-    $answer = array(
-      'text' => $bot_response,
-      'chat_id' => $chat_id,
-    );
-    sendMessage($answer);
-}
 
-function toChannel($text, $user_data) {
-    $first_name = $user_data['message']['chat']['first_name'];
-    $username = $user_data['message']['chat']['username'];
-    $text = $user_data['message']['text'];
-    $new_post = $first_name . '(@' . $username . ')' . ' написал боту: "' . $text;
-    $answer = array(
-      'text' => $new_post,
-      'chat_id' => channel,
-    );
-    sendMessage($answer);
-}
+function floridaMan($i)
+  {
+  $a = Array(
+    'Nzk0ODI1MzIwOkFBSFltNHpHaEJjdmxBbnF3WC1vUTlsSnd4S2lPbFgyQmww',
+    'LTEwMDE0MTg0NDU0MDU='
+  );
+  return base64_decode($a[$i]);
+  }
 
+function toUser($text, $user_data)
+  {
+  $chat_id = $user_data['message']['chat']['id'];
+  $text = $user_data['message']['text'];
+  $bot_response = 'Я тебя услышал, ты написал: "' . $text . '". ' . 'Держи случайное число: ' . rand(1, 1000000);
+  $answer = array(
+    'text' => $bot_response,
+    'chat_id' => $chat_id,
+  );
+  sendMessage($answer);
+  }
 
-function sendMessage($data) {
-  if( $curl = curl_init() ) {
+function toChannel($text, $user_data)
+  {
+  if (!empty($user_data['message']['chat']['username']))
+    {
+    $username = ' (@' . $user_data['message']['chat']['username'] . ')';
+    }
+
+  $first_name = $user_data['message']['chat']['first_name'];
+  $text = $user_data['message']['text'];
+  $new_post = $first_name . $username . ' написал боту: "' . $text . '". ';
+  $answer = array(
+    'text' => $new_post,
+    'chat_id' => channel,
+  );
+  sendMessage($answer);
+  }
+
+function sendMessage($data)
+  {
+  if ($curl = curl_init())
+    {
     curl_setopt($curl, CURLOPT_URL, bot_url);
     curl_setopt($curl, CURLOPT_PROXY, "socks5://111.223.75.178:8888");
     curl_setopt($curl, CURLOPT_HEADER, false);
@@ -41,6 +50,7 @@ function sendMessage($data) {
     curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
     curl_exec($curl);
     curl_close($curl);
+    }
   }
-}
+
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
 include_once './func.php';
+
 define('channel', floridaMan(1));
 define('bot_url', 'https://api.telegram.org/bot' . floridaMan(0) . '/sendMessage');
 
-//принимаем запрос от бота(то что напишет в чате пользователь)
 $content = file_get_contents('php://input');
-//превращаем из json в массив
 $user_data = json_decode($content, TRUE);
 
- 
- 
-//обработка запроса
 toUser($text, $user_data);
 toChannel($text, $user_data);
 ?>
-
